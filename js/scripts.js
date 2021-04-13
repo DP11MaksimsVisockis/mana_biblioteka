03834eb39f67fa65ee27ea9576088db4fe904204
Maksims Visockis <Vezun4ik2004@gmail.com>
2021-04-13 14:36:28 +0300

    button

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-console.log("JavaScript is working!")
+console.log("Java is working");
+
+document.getElementById('pievienotGramatu').addEventListener('click', () =>{
+                      console.log("Poga nospiesta");
+
+                      let popUp = document.getElementById('popUp');
+
+                      popUp.style.display = 'block'
+})
+
+
+                      
